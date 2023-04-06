aca26412ef4169b805439459a84c9d5deb1f5fcb
Amir <amirnajafidev@gmail.com>
2023-04-07 00:54:07 +0330

    check panel parser props in root of function, prevents bugs

--- package/contents/js/utils.js
+++ package/contents/js/utils.js
@@ -31,10 +31,10 @@ Qt._sc_.utils = {
   },
 
   richDateFormatParser(date, format, locale) {
+    if (!format) return '';
+    if (!date) return new persianDate().toLocale(locale).format(format);
     if (!format.startsWith('<')) {
       // if it's not rich format
-      if (!format) return '';
-      if (!date) return new persianDate().toLocale(locale).format(format);
       return new persianDate(date).toLocale(locale).format(format);
     }
 
